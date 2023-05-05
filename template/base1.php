7014753e1ec42f619dd21a9ad0ccd1e3e2913dd5
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-05-05 09:21:53 +0200

    feat: add rough base.php structure

---
+++ template/base1.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="it">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>NiceTrip</title>
+</head>
+<body>
+    <div class="container-fluid p-o overflow-hidden">
+        <div class="row">
+            <div class="col-12">
+                <header>
+                    <h1>Title</h1>
+                    <h2>Subtitle</h2>
+                </header>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <img class="img-fluid" src="group-travel.jpg" alt="" />
+            </div>
+            <div class="col-6">
+                <p>Example</p>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
