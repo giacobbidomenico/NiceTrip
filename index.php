b3ff2ab5fd3a94cd9bdba9c055e6356e3b4c805c
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-05-06 15:57:32 +0200

    fix: add text alternatives to the images and change the language of the index.php page to English

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     $templateParams["title"] = "NiceTrip";
     $templateParams["subtitle"] = "Share your travels with your friends";
     $templateParams["iconName"] = "icon.png";
+    $templateParams["iconDescription"] = "NiceTrip icon";
     $templateParams["mainImageName"] = "group-travel.png";
+    $templateParams["mainImageDescription"] = "2 people packing their bags to go on a trip";
     require("template/base1.php");
 ?>
