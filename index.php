f772165f9ebf931300f7b7821fff79385dca0909
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-05-06 12:50:16 +0200

    feat: develop the index.php page using base1.php as a template

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    require("bootstrap.php");
+    $templateParams["title"] = "NiceTrip";
+    $templateParams["subtitle"] = "Share your travels with your friends";
+    $templateParams["iconName"] = "icon.png";
+    $templateParams["mainImageName"] = "group-travel.png";
+    require("template/base1.php");
+?>
--- template/base1.php
+++ template/base1.php
@@ -4,24 +4,23 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>NiceTrip</title>
+    <title><?php echo $templateParams["title"];?></title>
 </head>
 <body>
     <div class="container-fluid p-0 overflow-hidden">
         <div class="row pt-3 ps-5 mb-2">
             <header>
-                <img  class="img-fluid float-start d-inlin-block" src="../upload/icon.png" alt="" />
-                <h1 class="fw-bold fs-1 d-inline-block text-primary">NiceTrip</h1>
-                <h2 class="fw-italic fs-6 text-muted">Share your travels with your friends</h2>
+                <img  class="img-fluid float-start d-inlin-block" src="<?php echo UPLOAD_DIR.$templateParams["iconName"]; ?>" alt="" />
+                <h1 class="fw-bold fs-1 d-inline-block text-primary"><?php echo $templateParams["title"];?></h1>
+                <h2 class="fw-italic fs-6 text-muted"><?php echo $templateParams["subtitle"];?></h2>
             </header>
         </div>
         <div class="row">
             <div class="col-md-1"></div>
             <div class="col-12 col-md-6 d-none d-md-block">
-                <img class="img-fluid rounded" src="../upload/group-travel.png" alt="" />
+                <img class="img-fluid rounded" src="<?php echo UPLOAD_DIR.$templateParams["mainImageName"]; ?>" alt="" />
             </div>
             <div class="col-12 col-md-5">
-                <p>Example</p>
             </div>
         </div>
     </div>
