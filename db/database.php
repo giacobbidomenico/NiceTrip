a2c772b35af099d7367d2442a101d0e2c9d67fb7
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-05-08 22:10:40 +0200

    feat: develop checkEmail method in DatabaseHelper class

--- db/database.php
+++ db/database.php
@@ -8,4 +8,14 @@ class DatabaseHelper{
             die("Connection failed: " . $db->connect_error);
         } 
     }
+
+    public function checkEmail($email) {
+        $query = "SELECT `users`.`id`, `users`.`userName`, `users`.`email` FROM `users` WHERE `users`.`email` = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 ?>
