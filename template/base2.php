f52a85e37fcc1a1dfbb513880123d3a2c6429c83
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-09 21:18:55 +0200

    feat: add profile link on desktop navbar

--- template/base2.php
+++ template/base2.php
@@ -85,6 +85,17 @@
 
                 <div class="row">
                     <div class="col-12 p-0 text-center text-xl-start">
+                        <a class="my-2 btn btn-light" href="<?php $navBarParams["profile"] ?>">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person mobile-icon" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                        </svg>
+                            <span class="d-none d-xl-inline">Profile</span>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 p-0 text-center text-xl-start">
                         <a class="my-2 btn btn-light" href="<?php $navBarParams["options"] ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-gear desktop-icon" viewBox="0 0 16 16">
                                 <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
