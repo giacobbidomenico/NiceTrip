43196034bcb569d98a514a139debc1ca67584325
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-12 16:42:56 +0200

    feat: add js

--- feed.php
+++ feed.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'bootstrap.php';
-$templateParams["title"] = "Blog TW - Contatti";
+$templateParams["title"] = "";
 $navBarParams["profile"] = "";
 $navBarParams["logOut"] = "";
 $navBarParams["feed"] = "";
@@ -9,7 +9,7 @@ $navBarParams["search"] = "";
 $navBarParams["notifications"] = "";
 $navBarParams["newPost"] = "";
 $navBarParams["options"] = "";
-$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","js/");
+$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","js/post-list.js");
 
 require 'template/base2.php';
 
