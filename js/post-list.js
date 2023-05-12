9d6f4ca1c630190653bc69fec307ea72140210a0
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-12 23:43:55 +0200

    fix: post parameters

--- js/post-list.js
+++ js/post-list.js
@@ -10,14 +10,16 @@ let index = 0;
         > evento:
             window.innerHeight + window.scrollY >= document.getElementById("mainArticle").offsetHeight + document.getElementById("mainArticle").scrollTop
     ? scroll verso l'alto ricarica la pagina
+    - alla visualizzazione di un post bisogna aggiornare il databse (entry che indichi che l'utente ha visualizzato il post)
  */
 
 
 axios.post('api-post-id-list.php', {}).then(response => {
     ids = response;
+    fillFeed();
 });
 
-window.addEventListener("onscroll", event => fillFeed());
+window.addEventListener("scroll", event => fillFeed());
 
 // checks whether the user has reached the bottom of the page
 function isBottomReached() {
@@ -26,19 +28,36 @@ function isBottomReached() {
 
 // adds a limited number of posts in mainArticle section
 function fillFeed() {
+    if (isBottomReached()) {
+        requestPostDetails(ids.data[0].id);
+        requestAuthorDetails(ids.data[0].id);
+        requestPostImages(ids.data[0].id);
+    }
 }
 
 // returns details of a given post
-function requestPostDetails() {
-
+function requestPostDetails(postId) {
+    const formData = new FormData();
+    formData.append('postId', postId);
+    axios.post('api-post-details.php', formData).then(response => {
+        console.log(response);
+    });
 }
 
 // returns images of a given post
-function requestPostImages() {
-
+function requestPostImages(postId) {
+    const formData = new FormData();
+    formData.append('postId', postId);
+    axios.post('api-post-details.php', formData).then(response => {
+        console.log(response);
+    });
 }
 
 // returns details of an author
-function requestAuthorDetails() {
-
+function requestAuthorDetails(follow) {
+    const formData = new FormData();
+    formData.append('followingUserId', follow);
+    axios.post('api-user-details-list.php', formData).then(response => {
+        console.log(response);
+    });
 }
