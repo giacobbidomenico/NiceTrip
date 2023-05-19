e1ca36be428b63e4192c1ded359eb3758c9a2bba
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-05-19 15:29:59 +0200

    feat: add documentation to database.php

--- db/database.php
+++ db/database.php
@@ -2,6 +2,20 @@
 class DatabaseHelper{
     private $db;
 
+    /**
+     * Buld a new DatabaseHelper.
+     * 
+     * @param $servername
+     *        address of the server
+     * @param $username
+     *        username to access the DB
+     * @param $password
+     *        password to access the DB
+     * @param $dbname
+     *        name of the database you want to access
+     * @param $port
+     *        port of the database server
+     */
     public function __construct($servername, $username, $password, $dbname, $port) {
         $this->db = new mysqli($servername, $username, $password, $dbname, $port);
         if ($this->db->connect_error) {
@@ -9,6 +23,12 @@ class DatabaseHelper{
         } 
     }
 
+    /**
+     * Check if email/username matches a user's account.
+     * 
+     * @param $email_username
+     *        user email/username       
+     */
     public function checkEmailOrUsername($email_username) {
         $query = "SELECT `users`.`id`, `users`.`userName`, `users`.`email` FROM `users` WHERE `users`.`email` = ? OR `users`.`username` = ?";
         $stmt = $this->db->prepare($query);
@@ -19,6 +39,12 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    /**
+     * Check if email/username matches a user's account.
+     * 
+     * @param $email
+     *        user email
+     */
     public function checkEmail($email) {
         $query = "SELECT `users`.`id`, `users`.`userName`, `users`.`email` FROM `users` WHERE `users`.`email` = ?";
         $stmt = $this->db->prepare($query);
@@ -29,6 +55,12 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    /**
+     * Check if username matches a user's account.
+     * 
+     * @param $username
+     *        user username
+     */
     public function checkUsername($username) {
         $query = "SELECT `users`.`id`, `users`.`userName`, `users`.`email` FROM `users` WHERE `users`.`username` = ?";
         $stmt = $this->db->prepare($query);
@@ -39,6 +71,12 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    /**
+     * Function that check if the user's account has been activated, by seeing if the activation_code field is NULL
+     * 
+     * @param $email_username
+     *        user email/username
+     */
     public function isAccountActivated($email_username) {
         $query = "SELECT `users`.id, `users`.`userName`, `users`.`email` FROM `users` WHERE (`users`.`email` = ? OR `users`.`userName` = ?) AND `users`.`activation_code` IS NULL";
         $stmt = $this->db->prepare($query);
@@ -49,6 +87,14 @@ class DatabaseHelper{
         return count($result->fetch_all(MYSQLI_ASSOC)) !== 0;
     }
 
+    /**
+     * Function that returns the user that corresponds to the credentials entered during login.
+     * 
+     * @param $email_username
+     *        user email/username
+     * @param $password
+     *        user password
+     */
     public function checkLogin($email_username, $password) {
         $query = "SELECT `users`.id, `users`.`userName`, `users`.`email` FROM `users` WHERE (`users`.`email` = ? OR `users`.`userName` = ?) AND `users`.`password` = ?";
 
@@ -58,8 +104,14 @@ class DatabaseHelper{
         $result = $stmt->get_result();
 
         return $result->fetch_all(MYSQLI_ASSOC);
+        
     }
 
+    /**
+     * Function that associates a new code to the user, which is also present in the cookie and will 
+     * allow him to maintain his session even after closing the browser.
+     * 
+     */
     public function updateSessionExtensionCode($session_extension_code, $user_id) {
         $query = "UPDATE `users` SET `cookie` = ? WHERE `users`.`id` = ?";
 
@@ -69,6 +121,11 @@ class DatabaseHelper{
         return $stmt->execute();
     }
 
+    /**
+     * Function that returns the user from a code associated with him and also present in a cookie in 
+     * the user's browser.
+     * 
+     */
     public function getUsersBySessionExtensionCode($session_extension_code) {
         $query = "SELECT `users`.`id`, `users`.`userName`, `users`.`email` FROM `users` WHERE `users`.`cookie` = ?";
 
@@ -80,6 +137,10 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    /**
+     * Function that returns the user corresponding to a code assigned to activate the account.
+     * 
+     */
     public function getUsersByActivationCode($activation_code) {
         $query = "SELECT `users`.`id`, `users`.`userName`, `users`.`email` FROM `users` WHERE `users`.`activation_code` = ?";
 
@@ -91,6 +152,10 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    /**
+     * Function that sign up a new user into the database.
+     * 
+     */
     public function signUpUser($username, $name, $last_name, $email, $password, $activation_code) {
         $query = 'INSERT INTO `users`(`userName`, `name`, `lastName`, `email`, `password`, `activation_code`) VALUES (?,?,?,?,?,?)';
 
