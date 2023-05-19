f007c5f80b670be9826942f5387d475d50e64acb
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-19 17:48:58 +0200

    fix: edit/delete tools disposal

--- js/post-preview.js
+++ js/post-preview.js
@@ -33,85 +33,89 @@ function Post(id, editable) {
 		this.likesNumber = postDetails.data[0].likeNumber;
 		console.log("liked: " + postDetails.data[0].liked);
 		this.like = postDetails.data[0].liked === 1 ? true : false;
-		let scheme = `<div class="row gy-4 my-3">
-		<article id="p-` + postDetails.data[0].id + `" class="card w-75 mx-auto p-0">
-			<div class="card-header">
-				<h4 id="p-` + postDetails.data[0].id + `-title" class="card-title">
-					<a id="post-title" class="link-dark link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="">` + postDetails.data[0].title + `</a>
-				</h4>
-				<div id="p-` + postDetails.data[0].id + `-author-details" class="">
-				</div>
-			</div>
-			<div id="p-` + postDetails.data[0].id + `-carousel" class="carousel slide">
-			</div>
-			<div class="container-fluid card-body gy-2">
-				<div class="row">
-					<div class="col">
-						<a id="p-` + postDetails.data[0].id + `-comments" class="btn btn-light my-2" href="">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-card-text desktop-icon" viewBox="0 0 16 16">
-								<path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
-								<path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z" />
-							</svg>
-							<span>
-								comments: ` + (postDetails.data[0].commentNumber !== 0 ? postDetails.data[0].commentNumber : 0) + `
-							</span>
-						</a>
-						<button id="p-` + postDetails.data[0].id + `-likes" class="btn btn-light my-2">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-balloon-heart desktop-icon ` + (this.like ? "d-none" : "") + `" viewBox="0 0 16 16">
-								<path fill-rule="evenodd" d="m8 2.42-.717-.737c-1.13-1.161-3.243-.777-4.01.72-.35.685-.451 1.707.236 3.062C4.16 6.753 5.52 8.32 8 10.042c2.479-1.723 3.839-3.29 4.491-4.577.687-1.355.587-2.377.236-3.061-.767-1.498-2.88-1.882-4.01-.721L8 2.42Zm-.49 8.5c-10.78-7.44-3-13.155.359-10.063.045.041.089.084.132.129.043-.045.087-.088.132-.129 3.36-3.092 11.137 2.624.357 10.063l.235.468a.25.25 0 1 1-.448.224l-.008-.017c.008.11.02.202.037.29.054.27.161.488.419 1.003.288.578.235 1.15.076 1.629-.157.469-.422.867-.588 1.115l-.004.007a.25.25 0 1 1-.416-.278c.168-.252.4-.6.533-1.003.133-.396.163-.824-.049-1.246l-.013-.028c-.24-.48-.38-.758-.448-1.102a3.177 3.177 0 0 1-.052-.45l-.04.08a.25.25 0 1 1-.447-.224l.235-.468ZM6.013 2.06c-.649-.18-1.483.083-1.85.798-.131.258-.245.689-.08 1.335.063.244.414.198.487-.043.21-.697.627-1.447 1.359-1.692.217-.073.304-.337.084-.398Z" />
-							</svg>
-							<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-balloon-heart-fill desktop-icon ` + (this.like ? "" : "d-none") + `" viewBox="0 0 16 16">
-								<path fill-rule="evenodd" d="M8.49 10.92C19.412 3.382 11.28-2.387 8 .986 4.719-2.387-3.413 3.382 7.51 10.92l-.234.468a.25.25 0 1 0 .448.224l.04-.08c.009.17.024.315.051.45.068.344.208.622.448 1.102l.013.028c.212.422.182.85.05 1.246-.135.402-.366.751-.534 1.003a.25.25 0 0 0 .416.278l.004-.007c.166-.248.431-.646.588-1.115.16-.479.212-1.051-.076-1.629-.258-.515-.365-.732-.419-1.004a2.376 2.376 0 0 1-.037-.289l.008.017a.25.25 0 1 0 .448-.224l-.235-.468ZM6.726 1.269c-1.167-.61-2.8-.142-3.454 1.135-.237.463-.36 1.08-.202 1.85.055.27.467.197.527-.071.285-1.256 1.177-2.462 2.989-2.528.234-.008.348-.278.14-.386Z" />
-							</svg>
-							<span>
-								likes: ` + (postDetails.data[0].likeNumber !== 0 ? postDetails.data[0].likeNumber : 0) + `
-							</span>
-						</button>`;
+		let scheme = `
+						<div class="row gy-4 my-3">
+							<article id="p-` + postDetails.data[0].id + `" class="card w-75 mx-auto p-0">
+								<div class="card-header">
+									<h4 id="p-` + postDetails.data[0].id + `-title" class="card-title">
+										<a id="post-title" class="link-dark link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="">` + postDetails.data[0].title + `</a>
+									</h4>
+									<div id="p-` + postDetails.data[0].id + `-author-details" class="">
+									</div>
+								</div>
+								<div id="p-` + postDetails.data[0].id + `-carousel" class="carousel slide">
+								</div>
+								<div class="container-fluid card-body gy-2">
+									<div class="row">
+										<div class="col">
+											<a id="p-` + postDetails.data[0].id + `-comments" class="btn btn-light my-2" href="">
+												<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-card-text desktop-icon" viewBox="0 0 16 16">
+													<path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+													<path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z" />
+												</svg>
+												<span>
+													comments: ` + (postDetails.data[0].commentNumber !== 0 ? postDetails.data[0].commentNumber : 0) + `
+												</span>
+											</a>
+											<button id="p-` + postDetails.data[0].id + `-likes" class="btn btn-light my-2">
+												<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-balloon-heart desktop-icon ` + (this.like ? "d-none" : "") + `" viewBox="0 0 16 16">
+													<path fill-rule="evenodd" d="m8 2.42-.717-.737c-1.13-1.161-3.243-.777-4.01.72-.35.685-.451 1.707.236 3.062C4.16 6.753 5.52 8.32 8 10.042c2.479-1.723 3.839-3.29 4.491-4.577.687-1.355.587-2.377.236-3.061-.767-1.498-2.88-1.882-4.01-.721L8 2.42Zm-.49 8.5c-10.78-7.44-3-13.155.359-10.063.045.041.089.084.132.129.043-.045.087-.088.132-.129 3.36-3.092 11.137 2.624.357 10.063l.235.468a.25.25 0 1 1-.448.224l-.008-.017c.008.11.02.202.037.29.054.27.161.488.419 1.003.288.578.235 1.15.076 1.629-.157.469-.422.867-.588 1.115l-.004.007a.25.25 0 1 1-.416-.278c.168-.252.4-.6.533-1.003.133-.396.163-.824-.049-1.246l-.013-.028c-.24-.48-.38-.758-.448-1.102a3.177 3.177 0 0 1-.052-.45l-.04.08a.25.25 0 1 1-.447-.224l.235-.468ZM6.013 2.06c-.649-.18-1.483.083-1.85.798-.131.258-.245.689-.08 1.335.063.244.414.198.487-.043.21-.697.627-1.447 1.359-1.692.217-.073.304-.337.084-.398Z" />
+												</svg>
+												<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-balloon-heart-fill desktop-icon ` + (this.like ? "" : "d-none") + `" viewBox="0 0 16 16">
+													<path fill-rule="evenodd" d="M8.49 10.92C19.412 3.382 11.28-2.387 8 .986 4.719-2.387-3.413 3.382 7.51 10.92l-.234.468a.25.25 0 1 0 .448.224l.04-.08c.009.17.024.315.051.45.068.344.208.622.448 1.102l.013.028c.212.422.182.85.05 1.246-.135.402-.366.751-.534 1.003a.25.25 0 0 0 .416.278l.004-.007c.166-.248.431-.646.588-1.115.16-.479.212-1.051-.076-1.629-.258-.515-.365-.732-.419-1.004a2.376 2.376 0 0 1-.037-.289l.008.017a.25.25 0 1 0 .448-.224l-.235-.468ZM6.726 1.269c-1.167-.61-2.8-.142-3.454 1.135-.237.463-.36 1.08-.202 1.85.055.27.467.197.527-.071.285-1.256 1.177-2.462 2.989-2.528.234-.008.348-.278.14-.386Z" />
+												</svg>
+												<span>
+													likes: ` + (postDetails.data[0].likeNumber !== 0 ? postDetails.data[0].likeNumber : 0) + `
+												</span>
+											</button>`;
 		if (this.editable) {
-			const modal = `<div id="exampleModal" class="modal" tabindex="-1">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title">Are you sure?</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-				</div>
-				<div class="modal-body">
-					<p>You cannot restore posts that have been deleted.</p>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Delete</button>
-				</div>
-			</div>
-		</div>
-	</div>`;
-			document.getElementById("p" + this.id).insertAdjacentHTML("beforebegin", modal);
-			scheme += `<div class="dropdown">
-				<button id="likes" class="btn btn-light my-2 dropdown-toggle" type="button" data-bs-toggle="dropdown">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-						<path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
-					</svg>
-				</button>
-				<ul class="dropdown-menu">
-					<li>
-						<form method="post" action="newpost.php" class="inline">
-							<input type="hidden" name="postToEdit" value="` + this.id + `">
-								<button type="submit" name="submit_param" value="submit_value" class="dropdown-item">
-									Edit post
-								</button>
-						</form>
-					</li>
-					<li>
-						<button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModal">
-							Delete post
-						</button>
-					</li>
-				</ul>
-			</div>`;
+			scheme = `
+				<div id="exampleModal" class="modal" tabindex="-1">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title">Are you sure?</h5>
+								<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+							</div>
+							<div class="modal-body">
+								<p>You cannot restore posts that have been deleted.</p>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+								<button type="button" class="btn btn-primary">Delete</button>
+							</div>
+						</div>
+					</div>
+				</div>` +
+				scheme +
+				`<div class="dropdown d-inline">
+					<button id="likes" class="btn btn-light my-2 dropdown-toggle" type="button" data-bs-toggle="dropdown">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-three-dots desktop-icon" viewBox="0 0 16 16">
+							<path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+						</svg>
+					</button>
+					<ul class="dropdown-menu">
+						<li>
+							<form method="post" action="newpost.php" class="inline">
+								<input type="hidden" name="postToEdit" value="` + this.id + `">
+									<button type="submit" name="submit_param" value="submit_value" class="dropdown-item">
+										Edit post
+									</button>
+							</form>
+						</li>
+						<li>
+							<button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModal">
+								Delete post
+							</button>
+						</li>
+					</ul>
+				</div>`;
         }
 		scheme += `</div></div></div></article></div>`;
 		document.getElementById("feed").insertAdjacentHTML("beforeend", scheme);
-		document.getElementById("p-" + postDetails.data[0].id + "-likes").addEventListener("click", event => { this.notifyLike();});
+		if (!this.editable) {
+			document.getElementById("p-" + postDetails.data[0].id + "-likes").addEventListener("click", event => { this.notifyLike(); });
+        }
 		console.log(document.getElementById("p-" + this.id + "-likes"));
 		//document.getElementById("p-" + this.id + "-likes").addEventListener("click", event => { console.log("click");});
 
@@ -137,19 +141,22 @@ function Post(id, editable) {
 	this.setPostImages = function(images) {
 		let scheme = `<div class="carousel-inner">`;
 		for (let i = 0; i < images.length; i++) {
-			scheme += `<div class="carousel-item ` + (i === 0 ? "active" : "") + `">
-				<img id="image" src="img/` + images[0].path + `" class="d-block w-100" alt="" /></div>`;
+			scheme += `
+				<div class="carousel-item ` + (i === 0 ? "active" : "") + `">
+					<img id="image" src="img/` + images[0].path + `" class="d-block w-100" alt="" />
+				</div>`;
 		}
 
-		scheme += `</div>
-		<button class="carousel-control-prev" type="button" data-bs-target="#p-` + this.id + `-carousel" data-bs-slide="prev">
-			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="visually-hidden">Previous</span>
-		</button> 
-		<button class="carousel-control-next" type="button" data-bs-target="#p-` + this.id + `-carousel" data-bs-slide="next">
-			<span class="carousel-control-next-icon" aria-hidden="true"></span>
-			<span class="visually-hidden">Next</span>
-		</button>`;
+		scheme += `
+				</div>
+				<button class="carousel-control-prev" type="button" data-bs-target="#p-` + this.id + `-carousel" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Previous</span>
+				</button> 
+				<button class="carousel-control-next" type="button" data-bs-target="#p-` + this.id + `-carousel" data-bs-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Next</span>
+				</button>`;
 		document.getElementById("p-" + this.id + "-carousel").innerHTML = scheme;
 		this.isCarouselAdded = true;
 	}
