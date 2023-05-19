8143b390c2a2883abff27f5c2ef6b6444f686d63
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-05-19 19:47:39 +0200

    feat: develop reporting-page.php

---
+++ template/reporting-page.php
@@ -0,0 +1,13 @@
+<div class="col-12">
+    <h3 class="<?php echo $templateParams['error'] ? 'text-danger' : 'text-primary'?> fw-bold fs-2 mt-3"><?php echo $templateParams["pageName"]; ?></h3>
+</div>
+
+<div class="col-12">
+    <p class="fw-bold"><?php echo $templateParams["message"];?></p>
+</div>
+
+<div class="col-12 mt-3">
+    <div class="col-md-6 d-grid pe-1">
+        <a href="index.php" class="btn btn-primary btn-lg rounded-0">Home</a>
+    </div>
+</div>
