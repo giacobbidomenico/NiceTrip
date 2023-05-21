71abf4540eba68e459faa9ea7724bd0a84f62118
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-05-21 20:01:40 +0200

    feat: insert the bing maps api to manage the autocomplete of the destinations

--- newpost.php
+++ newpost.php
@@ -18,8 +18,10 @@
 	$navBarParams["options"] = "";
     $templateParams["introduction"] = "New Post";
     $templateParams["mainArticle"] = 'post-creation.php';
-	$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js");
+	$templateParams["js"] = array(array("https://unpkg.com/axios/dist/axios.min.js"),
+								  array("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"),
+								  array("https://www.bing.com/api/maps/mapcontrol?key=ApfT2xvKFGN88pRenIxehg4qyZ5OULOFUQhbXvP3jVvShecLtAsdF1oDFUjmI2XA&callback=loadMapScenario", array("async", "defer")), 
+								  array("js/newpost.js"));
 
 	require 'template/base2.php';
-
 ?>
