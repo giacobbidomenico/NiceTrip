fb99414e244b467d9e757d72e1b61862c8ff6767
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-21 19:58:36 +0200

    feat: define horizontal introduction banner template

---
+++ template/profileIntroduction.php
@@ -0,0 +1,22 @@
+<div class="rounded-top text-dark d-flex flex-row bg-light" style="/* background-color: #000; */height:200px;">
+	<div class="ratio ratio-1x1 ms-4 mt-5 d-flex flex-column" style="width: 150px;">
+		<div class="p-0 d-flex align-items-center  mt-4 mb-2 border border-dark-subtle" style="z-index: 1;background-color: white;">
+			<img id="author-image" src="profilePhotos/" alt="Generic placeholder image" class="align-middle" style="width: 100%;/* padding-top: 17%; *//* overflow: hidden; */">
+		</div>
+	</div>
+	<div class="ms-3" style="margin-top: 130px;">
+		<h1 id="author-username" class="text-primary"></h1>
+
+	</div>
+</div>
+<div class="p-4 text-black" style="/* background-color: #f8f9fa; */">
+	<div class="d-flex justify-content-end text-center py-1">
+		<button id="b-follow" class="btn btn-primary my-2">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="desktop-icon bi bi-check-circle" viewBox="0 0 16 16">
+				<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+				<path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+			</svg>
+			<span></span>
+		</button>
+	</div>
+</div>
