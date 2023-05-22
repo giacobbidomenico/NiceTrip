29705a71498e85e7826948a93808ca558ca9daf2
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-22 13:42:29 +0200

    feat: modeling follow button

--- template/profileIntroduction.php
+++ template/profileIntroduction.php
@@ -11,12 +11,11 @@
 </div>
 <div class="p-4 text-black" style="/* background-color: #f8f9fa; */">
 	<div class="d-flex justify-content-end text-center py-1">
-		<button id="b-follow" class="btn btn-primary my-2">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="desktop-icon bi bi-check-circle" viewBox="0 0 16 16">
-				<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-				<path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-			</svg>
+		<button id="b-follow" class="btn  my-2">
 			<span></span>
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="desktop-icon bi bi-check" viewBox="0 0 16 16">
+				<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+			</svg>
 		</button>
 	</div>
 </div>
