ed04673597e1fd3e73a380def57835740c5be29a
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-22 18:19:24 +0200

    fix: make follower and follow lists scrollable

--- template/side-follow.php
+++ template/side-follow.php
@@ -1,43 +1,48 @@
 <?php
-	if(isset($_SESSION["id"])){
-		$followers = $dbh->getFollowers($_SESSION["id"]);
-		$follows = $dbh->getFollows($_SESSION["id"]);
-	}
+	$followers = $dbh->getFollowers($_GET["userProfile"]);
+	$follows = $dbh->getFollows($_GET["userProfile"]);
 ?>
 <div class="container-fluid">
 	<div class="row ">
-		<div class="card" style="width: 18rem; max-height:25%;">
-			<div class="card-body">
-				<h2 class="card-title">Followers: 31</h2>
+		<div class="col gy-4">
+			<div class="card follow-card">
+				<div class="card-body">
+					<h2 class="card-title">Follower: <?php echo count($followers)?></h2>
+				</div>
+				<ul class="list-group list-group-flush overflow-y-scroll">
+					<?php
+						foreach ($followers as $follower){
+							echo '<li class="list-group-item">
+									<div id="fer-'.$follower["id"].'" class="">
+										<img id="fer-'.$follower["id"].'-author-image" class="desktop-icon" src="profilePhotos/'.$follower["photoPath"].'" />
+										<a id="fer-'.$follower["id"].'author-username" class="link-secondary link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="profile.php?userProfile='.$follower["id"].'">'.$follower["userName"].'</a>
+									</div>
+								</li>';
+						}
+					?>
+				</ul>
 			</div>
-			<?php
-				foreach ($followers as $follower){
-
-				}
-			?>
-			<ul class="list-group list-group-flush overflow-y-scroll">
-				<li class="list-group-item">
-					<div id="" class="">
-						<img id="author-image" class="desktop-icon" src="profilePhotos/genericProfilePhoto.jpg" />
-						<a id="author-username" class="link-secondary link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="">Domebbi Giaconico</a>
-					</div>
-				</li>
-			</ul>
 		</div>
 	</div>
 	<div class="row ">
-		<div class="card" style="width: 18rem; max-height:25%;">
-			<div class="card-body">
-				<h2 class="card-title">Follow: </h2>
+		<div class="col gy-4">
+			<div class="card follow-card">
+				<div class="card-body">
+					<h2 class="card-title">Follow: <?php echo count($follows)?></h2>
+				</div>
+				<ul class="list-group list-group-flush overflow-y-scroll">
+					<?php
+						foreach ($follows as $follow){
+							echo '<li class="list-group-item">
+									<div id="fer-'.$follow["id"].'" class="">
+										<img id="fer-'.$follow["id"].'-author-image" class="desktop-icon" src="profilePhotos/'.$follow["photoPath"].'" />
+										<a id="fer-'.$follow["id"].'author-username" class="link-secondary link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="profile.php?userProfile='.$follow["id"].'">'.$follow["userName"].'</a>
+									</div>
+								</li>';
+						}
+					?>
+				</ul>
 			</div>
-			<ul class="list-group list-group-flush overflow-y-scroll">
-				<li class="list-group-item">
-					<div id="" class="">
-						<img id="author-image" class="desktop-icon" src="profilePhotos/genericProfilePhoto.jpg" />
-						<a id="author-username" class="link-secondary link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="">Domebbi Giaconico</a>
-					</div>
-				</li>
-			</ul>
 		</div>
 	</div>
 </div>
