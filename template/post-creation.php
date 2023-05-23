1bf62b315b44f8aff19f7d277ffd077486a8addb
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-05-23 16:26:48 +0200

    feat: development of the forms to enter the arrival and departure times of the destinations

--- template/post-creation.php
+++ template/post-creation.php
@@ -1,4 +1,4 @@
-<form class="needs-validation mt-4 ps-4 pe-4" method="POST" novalidate>
+<form class="g-3 mt-4 ps-4 pe-4" method="POST">
     <div class="row">
         <label for="email-username" class="form-label fw-bold fs-4 text-primary">Title</label>
         <input type="text" class="form-control" id="email-username" name="email-username" required>
@@ -6,40 +6,35 @@
         </div>
     </div>
 
-    <div class="row">
+    <div class="row mt-2">
         <label class="fw-bold fs-4 mb-1 text-primary" for="description">Description:</label>
         <textarea class="form-control" id="description" style="height: 100px"></textarea>
     </div>
 
-    <div class="row">
+    <div class="row mt-2">
         <div class="row">
-            <p  class="fw-bold fs-4 text-primary" >Destinations:</p>
-        </div>
-
-        <div class="row ps-3">
-            <div class="row">
+            <p class="fw-bold fs-4 text-primary">Destinations:</p>
+            <div class="col-3">
                 <label class="fw-bold" for="destination">Add destination:</label>
+                <input class="form-control" type='text' id='search-field' autocomplete="on"/>
             </div>
-            <div class="row">
-                <div id="search-field-container" class="col-4">
-                    <input class="form-control" type='text' id='search-field' autocomplete="on"/>
-                </div>
-
-
-                <div class="col-4 ms-1">
-                    <button class="btn btn-primary" type="button" id="add-destination-button">Add</button>
-                </div>
+            <div class="col-4">
+                <label class="fw-bold" for="start-time">Start Time:</label>
+                <input class="form-control" type='datetime-local' id='start-time' autocomplete="on"/>
             </div>
-        </div>
-        <div class="row">
-            <div id="destinations-table-container">
-                <p class="fw-bold fs-6 text-danger">For the moment there are no destinations</p>
+            <div class="col-4">
+                <label class="fw-bold" for="end-time">End Time:</label>
+                <input class="form-control" type='datetime-local' id='end-time' autocomplete="on"/>
             </div>
         </div>
-        
+        <div class="row mt-2">
+            <div class="col-1">
+                <button class="btn btn-primary" type="button" id="add-destination-button">Add</button>
+            </div>
+        </div>
     </div>
 
-    <div class="row">
+    <div class="row mt-5">
         <div class="col-9"></div>
         <div class="col-3 d-grid pe-1 float-end">
             <input class="btn btn-primary btn-lg" type="submit" value="Post" name="signup" id="post-submit" autocomplete="on">
