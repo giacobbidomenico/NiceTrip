1d82c5382a8f79c734276f07e4756508d89285f3
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-05-23 15:41:15 +0200

    fix: change layout of newpost.php page

--- template/post-creation.php
+++ template/post-creation.php
@@ -1,17 +1,17 @@
-<form class="row g-3 needs-validation mt-4" method="POST" novalidate>
-    <div class="col-12">
+<form class="needs-validation mt-4 ps-4 pe-4" method="POST" novalidate>
+    <div class="row">
         <label for="email-username" class="form-label fw-bold fs-4 text-primary">Title</label>
-        <input type="text" class="form-control rounded-0" id="email-username" name="email-username" required>
+        <input type="text" class="form-control" id="email-username" name="email-username" required>
         <div class="invalid-feedback">
         </div>
     </div>
 
-    <div class="col-12">
+    <div class="row">
         <label class="fw-bold fs-4 mb-1 text-primary" for="description">Description:</label>
         <textarea class="form-control" id="description" style="height: 100px"></textarea>
     </div>
-    
-    <div class="col-12">
+
+    <div class="row">
         <div class="row">
             <p  class="fw-bold fs-4 text-primary" >Destinations:</p>
         </div>
@@ -39,8 +39,9 @@
         
     </div>
 
-    <div class="col-12">
-        <div class="col-md-3 d-grid pe-1 float-end">
+    <div class="row">
+        <div class="col-9"></div>
+        <div class="col-3 d-grid pe-1 float-end">
             <input class="btn btn-primary btn-lg" type="submit" value="Post" name="signup" id="post-submit" autocomplete="on">
         </div>
     </div>
