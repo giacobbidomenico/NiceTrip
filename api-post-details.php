5d33ac07ac8d2f91a7635606fba81e31b7b92fdd
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-23 12:22:09 +0200

    feat: adds decorator to dbh object in case needed

--- api-post-details.php
+++ api-post-details.php
@@ -2,6 +2,7 @@
     require_once 'bootstrap.php';
     $id = $_SESSION["id"];
     $postId = $_POST["postId"];
+    $dbh = new checkFollowDecorator($dbh);
     $result = $dbh->getPostDetails($postId, $id);
     
     echo json_encode($result);
--- api-user-details-list.php
+++ api-user-details-list.php
@@ -2,7 +2,10 @@
     require_once 'bootstrap.php';
     $id = $_SESSION["id"];
     $userId = $_POST["userId"];
-    $result = $dbh->getPublicUserDetails($userId, $id, $_POST["checkFollow"] == "true" ? true : false);
+    if($_POST["checkFollow"] == "true"){
+        $dbh = new checkFollowDecorator($dbh);
+    }
+    $result = $dbh->getPublicUserDetails($userId, $id);
     
     echo json_encode($result);
 
