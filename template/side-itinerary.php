fe9ed84ee6e356577cb67d663cbf7b7347415dd4
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-24 11:40:22 +0200

    feat: define html scheme of itinerary template

---
+++ template/side-itinerary.php
@@ -0,0 +1,89 @@
+<div class="container-fluid">
+	<div class="row ">
+		<div class="col gy-4">
+			<div class="card" style="max-height:60%">
+				<ul class="list-group list-group-flush overflow-y-scroll">
+					<li class="list-group-item list-group-item-action active container-fluid">
+						<div class="row">
+							<p class="mb-1">22/22/2022</p>
+						</div>
+						<div class="row">
+							<div class="col-12 col-lg-9 pe-0">
+								<h5 class="mb-1">Babbi Cafè, Corso Giuseppe Mazzini, Cesena, FC</h5>
+							</div>
+							<div class="col-12 col-lg-3 p-lg-0">
+								<small>20:30 - 21:30</small>
+							</div>
+						</div>
+					</li>
+					<li class="list-group-item list-group-item-action container-fluid">
+						<div class="row">
+							<p class="mb-1">22/22/2022</p>
+						</div>
+						<div class="row">
+							<div class="col-12 col-lg-9 pe-0">
+								<h5 class="mb-1">Babbi Cafè, Corso Giuseppe Mazzini, Cesena, FC</h5>
+							</div>
+							<div class="col-12 col-lg-3 p-lg-0">
+								<small>20:30 - 21:30</small>
+							</div>
+						</div>
+					</li>
+					<li class="list-group-item list-group-item-action container-fluid">
+						<div class="row">
+							<p class="mb-1">22/22/2022</p>
+						</div>
+						<div class="row">
+							<div class="col-12 col-lg-9 pe-0">
+								<h5 class="mb-1">Babbi Cafè, Corso Giuseppe Mazzini, Cesena, FC</h5>
+							</div>
+							<div class="col-12 col-lg-3 p-lg-0">
+								<small>20:30 - 21:30</small>
+							</div>
+						</div>
+					</li>
+					<li class="list-group-item list-group-item-action container-fluid">
+						<div class="row">
+							<p class="mb-1">22/22/2022</p>
+						</div>
+						<div class="row">
+							<div class="col-12 col-lg-9 pe-0">
+								<h5 class="mb-1">Babbi Cafè, Corso Giuseppe Mazzini, Cesena, FC</h5>
+							</div>
+							<div class="col-12 col-lg-3 p-lg-0">
+								<small>20:30 - 21:30</small>
+							</div>
+						</div>
+					</li>
+					<li class="list-group-item list-group-item-action container-fluid">
+						<div class="row">
+							<p class="mb-1">22/22/2022</p>
+						</div>
+						<div class="row">
+							<div class="col-12 col-lg-9 pe-0">
+								<h5 class="mb-1">Babbi Cafè, Corso Giuseppe Mazzini, Cesena, FC</h5>
+							</div>
+							<div class="col-12 col-lg-3 p-lg-0">
+								<small>20:30 - 21:30</small>
+							</div>
+						</div>
+					</li>
+					<li class="list-group-item list-group-item-action container-fluid">
+						<div class="row">
+							<p class="mb-1">22/22/2022</p>
+						</div>
+						<div class="row">
+							<div class="col-12 col-lg-9 pe-0">
+								<h5 class="mb-1">Babbi Cafè, Corso Giuseppe Mazzini, Cesena, FC</h5>
+							</div>
+							<div class="col-12 col-lg-3 p-lg-0">
+								<small>20:30 - 21:30</small>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
