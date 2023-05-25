0f4f792fa2508add850e5049daff70834a74c80e
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-25 22:28:32 +0200

    fix: database method call parameters

--- api-post-like.php
+++ api-post-like.php
@@ -3,7 +3,7 @@
     $id = $_SESSION["id"];
     $postId = $_POST["postId"];
     $dbh = new checkFollowDecorator($dbh);
-    $result = $dbh->notifyLike($postId, $id, $_POST["like"] == "true" ? true : false);
+    $result = $dbh->notifyLike($postId, $id);
     
     echo json_encode($result);
 
