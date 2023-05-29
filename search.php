0671b3db7c4f70c5708b678c5eee2a35a8bd3509
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-05-29 21:56:29 +0200

    feat: define search page

---
+++ search.php
@@ -0,0 +1,29 @@
+<?php
+
+	require_once 'bootstrap.php';
+
+	//in case no session is active, redirects to login
+	if(!isSessionActive()){
+		header('Location: index.php');
+	}
+	if(!isset($_GET["userProfile"])){
+		$_GET["userProfile"] = $_SESSION["id"];
+	}
+
+	$templateParams["title"] = "YourProfile";
+	$templateParams["mainArticle"] = "search-template.php";
+	$navBarParams["profile"] = "";
+	$navBarParams["logOut"] = "";
+	$navBarParams["feed"] = "feed.php";
+	$navBarParams["search"] = "";
+	$navBarParams["notifications"] = "";
+	$navBarParams["newPost"] = "";
+	$navBarParams["options"] = "";
+	$templateParams["jsVars"] = array("const userProfile = ".$_GET["userProfile"]);
+	$templateParams["js"] = array(
+		"https://unpkg.com/axios/dist/axios.min.js",
+		"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js");
+
+	require 'template/base2.php';
+
+?>
