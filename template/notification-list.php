499073349bcfc2a7fba413bf8c46715e21b76049
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-06-09 17:01:18 +0200

    feat: development of the template for displaying the notification list

--- template/notification-list.php
+++ template/notification-list.php
@@ -1,11 +1,29 @@
 <div class="container-fluid mt-4">
 
-    <div class="row border-top border-bottom">
+    <div class="row border-top border-bottom pt-2 pb-2 mb-3">
         <div class="col-1">
-            <img class="img-fluid" src="<?php echo UPLOAD_DIR.'message-notification.png';?>"/>
+            <img class="img-fluid" src="<?php echo UPLOAD_DIR.'comment-icon.png';?>"/>
         </div>
-        <div class="col-4">
-            <p class="fw-bold">fcmrdigjmfioe</p>
+        <div class="col-10">
+            <p class="fs-5"><span class="fw-bold">john</span> commented on <a href="">your post</a></p>
+        </div>
+    </div>
+
+    <div class="row border-top border-bottom pt-2 pb-2 mb-3">
+        <div class="col-1">
+            <img class="img-fluid" src="<?php echo UPLOAD_DIR.'like-icon.png';?>"/>
+        </div>
+        <div class="col-10">
+            <p class="fs-5"><span class="fw-bold">john</span> liked <a href="">your post</a></p>
+        </div>
+    </div>
+    
+    <div class="row border-top border-bottom pt-2 pb-2 mb-3">
+        <div class="col-1">
+            <img class="img-fluid" src="<?php echo UPLOAD_DIR.'follow-icon.png';?>"/>
+        </div>
+        <div class="col-10">
+            <p class="fs-5"><span class="fw-bold">john</span> started following you</p>
         </div>
     </div>
 
