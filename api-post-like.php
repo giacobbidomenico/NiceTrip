e75341587a309c0200b089f9f603ada1e9e8d423
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-06-10 00:11:25 +0200

    fix: don't send a notification when the like is undone

--- api-post-like.php
+++ api-post-like.php
@@ -3,7 +3,9 @@
     $id = $_SESSION["id"];
     $postId = $_POST["postId"];
     $result = $dbh->notifyLike($postId, $id);
-    $dbh->insertLikeNotification($postId, $id);
+    if($result["insert"]){
+        $dbh->insertLikeNotification($postId, $id);
+    }
     
     header('Content-Type: application/json');
     echo json_encode($result);
