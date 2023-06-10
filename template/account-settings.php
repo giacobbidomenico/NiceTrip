e26c8280bc3693174f20aa55b7d7e73a7ffee97e
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-06-10 22:31:07 +0200

    feat: define account settings page elements

---
+++ template/account-settings.php
@@ -0,0 +1,136 @@
+<?php 
+    $id = $_SESSION["id"];
+    $userDetails = ($dbh->getPublicUserDetails($id, $id))[0];
+    //var_dump($userDetails);
+?>
+<form>
+    <div class="container-fluid">
+        <div class="row my-3">
+            <div class="col-4">
+                <div class="">
+				    <div class="ratio ratio-1x1">
+					    <div class="border d-flex align-items-center">
+						    <img id="c-34-image" src="http://localhost/tecnologieweb/NiceTrip/profilePhotos/genericProfilePhoto.jpg" class="img-fluid" alt="...">
+					    </div>
+				    </div>
+			    </div>
+            </div>
+            <div class="col">
+                <div class="">
+                    <label for="formFile" class="form-label">Profile Image</label>
+                    <input class="form-control" type="file" id="formFile">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col mb-3">
+                <label for="s-input" class="form-label">Name</label>
+                <input id="s-input" type="text" class="form-control" placeholder="<?php echo $userDetails["email"] ?>" disabled readonly>
+            </div>
+            <div class="col-auto d-flex align-items-end mb-3">
+                <button id="p-125-likes" class="btn btn-light">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="desktop-icon bi bi-pencil-square" viewBox="0 0 16 16">
+						<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
+						<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"></path>
+					</svg>
+					<span>
+						Edit post
+					</span>
+				</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col mb-3">
+                <label for="s-input" class="form-label">Last Name</label>
+                <input id="s-input" type="text" class="form-control" placeholder="<?php echo $userDetails["email"] ?>" disabled readonly>
+            </div>
+            <div class="col-auto d-flex align-items-end mb-3">
+                <button id="p-125-likes" class="btn btn-light">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="desktop-icon bi bi-pencil-square" viewBox="0 0 16 16">
+						<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
+						<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"></path>
+					</svg>
+					<span>
+						Edit post
+					</span>
+				</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col mb-3">
+                <label for="s-input" class="form-label">UserName</label>
+                <input id="s-input" type="text" class="form-control" placeholder="<?php echo $userDetails["email"] ?>" disabled readonly>
+            </div>
+            <div class="col-auto d-flex align-items-end mb-3">
+                <button id="p-125-likes" class="btn btn-light">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="desktop-icon bi bi-pencil-square" viewBox="0 0 16 16">
+						<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
+						<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"></path>
+					</svg>
+					<span>
+						Edit post
+					</span>
+				</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col mb-3">
+                <label for="s-input" class="form-label">Email</label>
+                <input id="s-input" type="text" class="form-control" placeholder="<?php echo $userDetails["email"] ?>" disabled readonly>
+            </div>
+            <div class="col-auto d-flex align-items-end mb-3">
+                <button id="p-125-likes" class="btn btn-light">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="desktop-icon bi bi-pencil-square" viewBox="0 0 16 16">
+						<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
+						<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"></path>
+					</svg>
+					<span>
+						Edit post
+					</span>
+				</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col mb-3">
+                <label for="s-input" class="form-label">Password</label>
+                <input id="s-input" type="text" class="form-control" placeholder="<?php echo $userDetails["email"] ?>" disabled readonly>
+            </div>
+            <div class="col-auto d-flex align-items-end mb-3">
+                <button id="p-125-likes" class="btn btn-light">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="desktop-icon bi bi-pencil-square" viewBox="0 0 16 16">
+						<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
+						<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"></path>
+					</svg>
+					<span>
+						Edit post
+					</span>
+				</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col mb-3">
+                <label for="s-input" class="form-label">Password</label>
+                <input id="s-input" type="text" class="form-control" placeholder="<?php echo $userDetails["email"] ?>" disabled readonly>
+            </div>
+            <div class="col-auto d-flex align-items-end mb-3">
+                <button id="" class="btn btn-light">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="desktop-icon bi bi-pencil-square" viewBox="0 0 16 16">
+						<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
+						<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"></path>
+					</svg>
+					<span>
+						Edit post
+					</span>
+				</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button id="" class="btn btn-light">
+					Apply
+				</button>
+            </div>
+        </div>
+    </div>
+</form>
+
