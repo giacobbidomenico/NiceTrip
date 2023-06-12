bfb329cd0a080372679d3c027df452519ee22f31
domenico.giacobbi <domenico.giacobbi@studio.unibo.it>
2023-06-12 11:59:51 +0200

    fix: don't show the arrows to scroll through the images if there is only one image in the posts

--- js/post-preview.js
+++ js/post-preview.js
@@ -148,13 +148,16 @@ function Post(id, editable) {
 				</div>`;
 		}
 
-		scheme += `
+		if(images.length <= 1) {
+			scheme += '</div>';
+		} else {
+			scheme += `
 				</div>
 				<button class="carousel-control-prev bg-" type="button" data-bs-target="#p-` + this.id + `-carousel" data-bs-slide="prev">
 					<span aria-hidden="true">
 						<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#0d6efd" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
 							<path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-				  		</svg>
+						</svg>
 					</span>
 					<span class="visually-hidden">Previous</span>
 				</button> 
@@ -162,11 +165,14 @@ function Post(id, editable) {
 					<span aria-hidden="true">
 						<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#0d6efd" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
 							<path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-				  		</svg>
+						</svg>
 					</span>
 					<span class="visually-hidden">Next</span>
 				</button>`;
+		}
+		
 		document.getElementById("p-" + this.id + "-carousel").innerHTML = scheme;
+
 		this.isCarouselAdded = true;
 	}
 
