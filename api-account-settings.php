58acd6c214b754890a223678df6d727f966d1470
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-06-13 09:31:59 +0200

    fix: server side user details update

--- api-account-settings.php
+++ api-account-settings.php
@@ -5,25 +5,27 @@
         $result["username"] = $dbh->editUserName($id, $_POST["username"]);
     }
     if(isset($_POST["password"])){
-        $result["password"] = $dbh->editUserPassword($userId, $_POST["password"]);
+        $result["password"] = $dbh->editUserPassword($id, $_POST["password"]);
     }
     if(isset($_POST["email"])){
-        $result["email"] = $dbh->editUserEmail($userId, $_POST["email"]);
+        $result["email"] = $dbh->editUserEmail($id, $_POST["email"]);
     }
     if(isset($_FILES["image"])){
         $allowedTypes = ['jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' , 'ppt'];
         $fileName = $_FILES["image"]["name"];
         $tmpName = $_FILES["image"]["tmp_name"];
             
-        $extension = strtolower(pathinfo($fileNames[$i], PATHINFO_EXTENSION));
+        $extension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
         if(in_array($extension, $allowedTypes)) {
             do {
                 $newFileName = random_str() . '.' .$extension;
-                $newFilePath = IMAGES_DIR . $newFileName;
+                $newFilePath = PROFILE_IMAGES_DIR . $newFileName;
             }while(file_exists($newFilePath));
 
-            if(move_uploaded_file($tmpNames[$i], $newFilePath)) {
-               $result["image"] = $dbh->editUserImageProfile($userId, $imageName);
+            if(move_uploaded_file($tmpName, $newFilePath)) {
+                //$result["image"] = var_dump($dbh->insertImage($postId, $newFileName));
+                $dbh->editUserImageProfile($id, $newFileName);
+                $result["image"] = $newFileName;
             }
         } else {
             $result["error"] = true;
