1ac7995e386dc3e9238121b0a2e3b6d33bd7f270
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-06-16 01:00:24 +0200

    fix: like button on post visualization page

--- template/post-visualization.php
+++ template/post-visualization.php
@@ -10,18 +10,18 @@
 <div class="col">
 	<article class="mx-3">
         <header class="my-4">
-            <h2 class=""><?php echo $postDetails[0]["title"]?></h2>
+            <h2 class=""><?php echo $postDetails[0]['title']?></h2>
 			<section class="my-3 d-flex">
 				<!-- Author details -->
 				<div id="p-131-author-details" class="">
 					<div id="p-131-author" class="d-flex flex-row">
 						<div class="ratio ratio-1x1 square-desktop-icon align-self-end">
 							<div class="border d-flex align-items-center">
-								<img src="profilePhotos/<?php echo $authorDetails[0]["photoPath"]?>" class="img-fluid mx-auto align-middle profile-image-introduction" alt="User profile image"/>
+								<img src="profilePhotos/<?php echo $authorDetails[0]['photoPath']?>" class="img-fluid mx-auto align-middle profile-image-introduction" alt="User profile image"/>
 							</div>
 						</div>
 						<h3 class="ms-3 mb-0 fs-6">
-							<a id="author-username" class="link-secondary link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="profile.php?userProfile=<?php echo $authorDetails[0]["id"]?>"><?php echo $authorDetails[0]["userName"]?></a>
+							<a id="author-username" class="link-secondary link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="profile.php?userProfile=<?php echo $authorDetails[0]['id']?>"><?php echo $authorDetails[0]['userName']?></a>
 						</h3>
 					</div>
 				</div>
@@ -86,15 +86,15 @@
 			</div>
 			<div class="row">
 				<div class="col">
-					<button id="p-likes" class="float-end btn btn-light my-2" <?php echo ($postDetails[0]["id"] === $_SESSION["id"])? "" : "disabled"?>>
-						<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-balloon-heart desktop-icon <?php echo ($postDetails[0]["liked"] == 1 ? "d-none" : "") ?>" viewBox="0 0 16 16">
+					<button id="p-likes" class="float-end btn btn-light my-2" <?php echo ($postDetails[0]['userId'] === $_SESSION['id'])? 'disabled' : ''?>>
+						<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-balloon-heart desktop-icon <?php echo ($postDetails[0]['liked'] == 1 ? 'd-none' : '') ?>" viewBox="0 0 16 16">
 							<path fill-rule="evenodd" d="m8 2.42-.717-.737c-1.13-1.161-3.243-.777-4.01.72-.35.685-.451 1.707.236 3.062C4.16 6.753 5.52 8.32 8 10.042c2.479-1.723 3.839-3.29 4.491-4.577.687-1.355.587-2.377.236-3.061-.767-1.498-2.88-1.882-4.01-.721L8 2.42Zm-.49 8.5c-10.78-7.44-3-13.155.359-10.063.045.041.089.084.132.129.043-.045.087-.088.132-.129 3.36-3.092 11.137 2.624.357 10.063l.235.468a.25.25 0 1 1-.448.224l-.008-.017c.008.11.02.202.037.29.054.27.161.488.419 1.003.288.578.235 1.15.076 1.629-.157.469-.422.867-.588 1.115l-.004.007a.25.25 0 1 1-.416-.278c.168-.252.4-.6.533-1.003.133-.396.163-.824-.049-1.246l-.013-.028c-.24-.48-.38-.758-.448-1.102a3.177 3.177 0 0 1-.052-.45l-.04.08a.25.25 0 1 1-.447-.224l.235-.468ZM6.013 2.06c-.649-.18-1.483.083-1.85.798-.131.258-.245.689-.08 1.335.063.244.414.198.487-.043.21-.697.627-1.447 1.359-1.692.217-.073.304-.337.084-.398Z"></path>
 						</svg>
-						<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-balloon-heart-fill desktop-icon <?php echo ($postDetails[0]["liked"] == 1 ? "" : "d-none") ?>" viewBox="0 0 16 16">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-balloon-heart-fill desktop-icon <?php echo ($postDetails[0]['liked'] == 1 ? '' : 'd-none') ?>" viewBox="0 0 16 16">
 							<path fill-rule="evenodd" d="M8.49 10.92C19.412 3.382 11.28-2.387 8 .986 4.719-2.387-3.413 3.382 7.51 10.92l-.234.468a.25.25 0 1 0 .448.224l.04-.08c.009.17.024.315.051.45.068.344.208.622.448 1.102l.013.028c.212.422.182.85.05 1.246-.135.402-.366.751-.534 1.003a.25.25 0 0 0 .416.278l.004-.007c.166-.248.431-.646.588-1.115.16-.479.212-1.051-.076-1.629-.258-.515-.365-.732-.419-1.004a2.376 2.376 0 0 1-.037-.289l.008.017a.25.25 0 1 0 .448-.224l-.235-.468ZM6.726 1.269c-1.167-.61-2.8-.142-3.454 1.135-.237.463-.36 1.08-.202 1.85.055.27.467.197.527-.071.285-1.256 1.177-2.462 2.989-2.528.234-.008.348-.278.14-.386Z"></path>
 						</svg>
 						<span>
-							likes: <?php echo $postDetails[0]["likeNumber"]?>
+							likes: <?php echo $postDetails[0]['likeNumber']?>
 						</span>
 					</button>
 				</div>
