e859a35f3a201eff8f0c524846a1d93ca1846c04
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-06-16 09:22:46 +0200

    feat: fills database with examples

--- db/nicetrip.sql
+++ db/nicetrip.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Giu 12, 2023 alle 22:12
--- Versione del server: 10.4.28-MariaDB
--- Versione PHP: 8.2.4
+-- Creato il: Giu 16, 2023 alle 09:21
+-- Versione del server: 10.4.25-MariaDB
+-- Versione PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,24 +36,17 @@ CREATE TABLE `comments` (
   `time` time NOT NULL,
   `postsId` int(11) NOT NULL,
   `userId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `comments`
 --
 
 INSERT INTO `comments` (`id`, `description`, `date`, `time`, `postsId`, `userId`) VALUES
-(33, 'copdvmpfdm', '2023-06-10', '11:01:07', 124, 3),
-(35, 'bgrt brtnr', '2023-06-10', '16:24:09', 128, 5),
-(44, 'voidfniobion', '2023-06-10', '22:35:44', 128, 3),
-(47, 'bel post', '2023-06-11', '15:55:32', 127, 12),
-(52, 'frwmpgvkne', '2023-06-12', '20:57:53', 127, 3),
-(53, 'jvbpibjffr', '2023-06-12', '21:21:05', 127, 3),
-(54, 'vjpirjvipoejbi', '2023-06-12', '22:01:02', 127, 3),
-(55, 'vjpirjvipoejbi', '2023-06-12', '22:03:00', 127, 3),
-(56, 'btepmhbem', '2023-06-12', '22:05:42', 127, 3),
-(57, 'tbjie\'pboj', '2023-06-12', '22:05:54', 127, 3),
-(58, 'colletta è bello', '2023-06-12', '22:06:22', 127, 3);
+(92, 'Lorem ipsum dolor sit amet.', '2023-06-15', '23:44:36', 135, 13),
+(93, 'Lorem ipsum dolor sit amet, consectetur ', '2023-06-16', '00:04:16', 135, 14),
+(94, 'Commento', '2023-06-16', '00:26:32', 136, 14),
+(95, 'Commento', '2023-06-16', '00:34:50', 139, 14);
 
 -- --------------------------------------------------------
 
@@ -65,7 +58,37 @@ CREATE TABLE `destinations` (
   `id` int(11) NOT NULL,
   `description` varchar(200) NOT NULL,
   `post_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `destinations`
+--
+
+INSERT INTO `destinations` (`id`, `description`, `post_id`) VALUES
+(53, 'Milano, Milano, Italia', 135),
+(54, 'Firenze, Firenze, Italia', 135),
+(55, 'Bologna, Bologna, Italia', 135),
+(56, 'Torino, Torino, Italia', 135),
+(57, 'Roma, Roma, Italia', 135),
+(58, 'Venezia, Venezia, Italia', 135),
+(59, 'Trieste, Trieste, Italia', 135),
+(60, 'Perugia, Perugia, Italia', 135),
+(61, 'Ancona, Ancona, Italia', 135),
+(62, 'Napoli, Napoli, Italia', 135),
+(75, 'Milano, Milano, Italia', 136),
+(76, 'Firenze, Firenze, Italia', 136),
+(77, 'Bologna, Bologna, Italia', 136),
+(78, 'Torino, Torino, Italia', 136),
+(79, 'Roma, Roma, Italia', 136),
+(80, 'Venezia, Venezia, Italia', 136),
+(81, 'Trieste, Trieste, Italia', 136),
+(82, 'Perugia, Perugia, Italia', 136),
+(83, 'Ancona, Ancona, Italia', 136),
+(84, 'Napoli, Napoli, Italia', 136),
+(85, 'Recanati, Macerata, Italia', 137),
+(86, 'Cesena, Forlì-Cesena, Italia', 137),
+(87, 'Alma Mater Studiorum - Università Di Bologna, Via Zamboni, 33, 40126 Bologna, BO, Italia', 137),
+(90, 'Cesena, Forlì-Cesena, Italia', 139);
 
 -- --------------------------------------------------------
 
@@ -77,17 +100,16 @@ CREATE TABLE `follows` (
   `id` int(11) NOT NULL,
   `follower` int(11) NOT NULL,
   `following` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `follows`
 --
 
 INSERT INTO `follows` (`id`, `follower`, `following`) VALUES
-(16, 3, 5),
-(48, 3, 12),
-(44, 5, 12),
-(17, 12, 5);
+(83, 13, 14),
+(88, 14, 13),
+(89, 14, 15);
 
 -- --------------------------------------------------------
 
@@ -99,17 +121,18 @@ CREATE TABLE `images` (
   `id` int(11) NOT NULL,
   `postsId` int(11) NOT NULL,
   `name` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `images`
 --
 
 INSERT INTO `images` (`id`, `postsId`, `name`) VALUES
-(110, 127, 'c2213cc523879e2ac6f2c63079c721.png'),
-(111, 127, '1aee65e4d454a4414668e5e652819f.png'),
-(112, 127, '0a67105e45bb60b0b2fc4b0145f41d.gif'),
-(113, 127, '05d2969291fc75e7fe241ac7fbcf23.png');
+(121, 135, '9cbf52b2051d84950584ae4b7a52e4.jpg'),
+(122, 136, '9cbf52b2051d84950584ae4b7a52e4.jpg'),
+(123, 137, 'a1c41e351648d5e3cc2dc856d2bb13.jpg'),
+(125, 139, '49e4f88869f60b97e5d197f5eb77d8.jpg'),
+(126, 136, '9cbf52b2051d84950584ae4b7a52e4.jpg');
 
 -- --------------------------------------------------------
 
@@ -120,18 +143,15 @@ INSERT INTO `images` (`id`, `postsId`, `name`) VALUES
 CREATE TABLE `likes` (
   `userId` int(11) NOT NULL,
   `postsId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `likes`
 --
 
 INSERT INTO `likes` (`userId`, `postsId`) VALUES
-(3, 124),
-(3, 127),
-(3, 128),
-(5, 127),
-(12, 124);
+(14, 135),
+(14, 136);
 
 -- --------------------------------------------------------
 
@@ -147,24 +167,26 @@ CREATE TABLE `notifications` (
   `postId` int(11) DEFAULT NULL,
   `datetime` datetime NOT NULL,
   `sent` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `notifications`
 --
 
 INSERT INTO `notifications` (`id`, `type`, `senderId`, `receiverId`, `postId`, `datetime`, `sent`) VALUES
-(154, 3, 3, 12, NULL, '2023-06-12 21:38:27', 1),
-(155, 3, 3, 12, NULL, '2023-06-12 21:39:17', 1),
-(156, 3, 3, 12, NULL, '2023-06-12 22:00:01', 1),
-(157, 1, 3, 12, 127, '2023-06-12 22:00:19', 1),
-(158, 1, 3, 12, 127, '2023-06-12 22:00:42', 1),
-(159, 2, 3, 12, 127, '2023-06-12 22:01:02', 1),
-(160, 1, 3, 12, 127, '2023-06-12 22:02:57', 1),
-(161, 2, 3, 12, 127, '2023-06-12 22:03:00', 1),
-(162, 2, 3, 12, 127, '2023-06-12 22:05:42', 1),
-(163, 2, 3, 12, 127, '2023-06-12 22:05:54', 1),
-(164, 2, 3, 12, 127, '2023-06-12 22:06:22', 1);
+(620, 1, 14, 15, 139, '2023-06-16 00:34:13', 1),
+(621, 2, 14, 15, 139, '2023-06-16 00:34:50', 1),
+(622, 1, 14, 15, 139, '2023-06-16 00:42:14', 1),
+(623, 1, 14, 15, 139, '2023-06-16 00:42:16', 1),
+(624, 1, 14, 15, 139, '2023-06-16 00:56:43', 1),
+(625, 1, 14, 15, 139, '2023-06-16 00:56:45', 1),
+(626, 1, 14, 15, 139, '2023-06-16 00:56:46', 1),
+(627, 1, 14, 15, 139, '2023-06-16 00:56:47', 1),
+(628, 1, 14, 15, 139, '2023-06-16 00:56:48', 1),
+(629, 1, 14, 15, 139, '2023-06-16 00:59:52', 1),
+(630, 1, 14, 15, 139, '2023-06-16 00:59:53', 1),
+(631, 1, 14, 15, 139, '2023-06-16 00:59:54', 1),
+(632, 1, 15, 13, 136, '2023-06-16 09:19:40', 1);
 
 -- --------------------------------------------------------
 
@@ -179,16 +201,17 @@ CREATE TABLE `posts` (
   `userId` int(11) NOT NULL,
   `time` time NOT NULL,
   `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `posts`
 --
 
 INSERT INTO `posts` (`id`, `title`, `description`, `userId`, `time`, `date`) VALUES
-(124, 'vnroingvo', 'nvrejngbogno', 5, '18:14:04', '2023-06-09'),
-(127, 'ciao', 'ciao', 12, '15:02:04', '2023-06-10'),
-(128, 'gegethrhe', 'gtntrnnntnrt', 5, '16:23:57', '2023-06-10');
+(135, 'My trip sample', 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\nLorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\nLorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\nLorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\nLorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 13, '23:44:12', '2023-06-15'),
+(136, 'My trip sample 2', 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\nLorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\nLorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\nLorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\nLorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 13, '23:44:12', '2023-06-15'),
+(137, 'My vacation', 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\nLorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 14, '00:03:45', '2023-06-16'),
+(139, 'NiceTrip', 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 15, '00:33:22', '2023-06-16');
 
 -- --------------------------------------------------------
 
@@ -206,18 +229,16 @@ CREATE TABLE `users` (
   `activation_code` varchar(30) DEFAULT NULL,
   `cookie` varchar(30) NOT NULL,
   `photoPath` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `users`
 --
 
 INSERT INTO `users` (`id`, `userName`, `name`, `lastName`, `email`, `password`, `activation_code`, `cookie`, `photoPath`) VALUES
-(3, 'primo', 'primo', 'primo', 'primo@primo.primo', '$2y$10$3wK7rbaNToUFSQxDnLxvz.TzOb.eru.XtYTXPkHct0OlT0ZgqkOAm', NULL, '3ffd95225a5e124286fac6e1bf2d2f', 'genericProfilePhoto.jpg'),
-(5, 'secondo', 'secondo', 'secondo', 'secondo@secondo.secondo', '$2y$10$3wK7rbaNToUFSQxDnLxvz.TzOb.eru.XtYTXPkHct0OlT0ZgqkOAm', NULL, '', 'genericProfilePhoto.jpg'),
-(6, 'terzo', 'terzo', 'terzo', 'terzo@terzo.terzo', '$2y$10$3wK7rbaNToUFSQxDnLxvz.TzOb.eru.XtYTXPkHct0OlT0ZgqkOAm', NULL, '', 'genericProfilePhoto.jpg'),
-(7, 'quarto', 'quarto', 'quarto', 'quarto@quarto.quarto', '$2y$10$3wK7rbaNToUFSQxDnLxvz.TzOb.eru.XtYTXPkHct0OlT0ZgqkOAm', NULL, '', 'genericProfilePhoto.jpg'),
-(12, 'cruciani', 'roberto', 'cruciani', 'nicetrip.socialplatform@gmail.com', '$2y$10$WANP7LzfMGZ.kk02K3ALfOKsoUC9uIYTc55KqXfpNeig8j9d3dAiq', NULL, '', '');
+(13, 'Lorenzo', 'Lorenzo', 'Colletta', 'lorenzo@mail.mail', '$2y$10$kbyLnFdAsVse6TtBWsGQRO1t76Nd.juHQY0BO4cbwhpyUbAjbUEx2', NULL, '', 'e45043818a1f030f3f566c267b97f3.png'),
+(14, 'Domenico Francesco', 'Domenico', 'Giacobbi', 'domenico@mail.mail', '$2y$10$zMqzgJucOUGXRUuyMIk1dOiFy2ByiGFIj6o6fneHZFMtDTCs0tDo2', NULL, '', '58f59d8e97c66edb36a8c50f3c2ad6.jpg'),
+(15, 'NiceTrip', 'nice', 'trip', 'nicetrip.social@outlook.com', '$2y$10$xCKVuqLud7ubXTYHL9kgEuSTVuYCwK898inSyVBufGqA9BbMdNbUa', NULL, '', 'genericProfilePhoto.jpg');
 
 -- --------------------------------------------------------
 
@@ -229,19 +250,17 @@ CREATE TABLE `visualizations` (
   `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `postId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `visualizations`
 --
 
 INSERT INTO `visualizations` (`id`, `userId`, `postId`) VALUES
-(370, 3, 124),
-(371, 3, 128),
-(372, 12, 124),
-(373, 12, 128),
-(374, 3, 127),
-(375, 5, 127);
+(446, 14, 135),
+(447, 13, 137),
+(448, 14, 136),
+(449, 14, 139);
 
 --
 -- Indici per le tabelle scaricate
@@ -325,49 +344,49 @@ ALTER TABLE `visualizations`
 -- AUTO_INCREMENT per la tabella `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
 
 --
 -- AUTO_INCREMENT per la tabella `destinations`
 --
 ALTER TABLE `destinations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
 
 --
 -- AUTO_INCREMENT per la tabella `follows`
 --
 ALTER TABLE `follows`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 
 --
 -- AUTO_INCREMENT per la tabella `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=117;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=127;
 
 --
 -- AUTO_INCREMENT per la tabella `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=165;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=633;
 
 --
 -- AUTO_INCREMENT per la tabella `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=140;
 
 --
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT per la tabella `visualizations`
 --
 ALTER TABLE `visualizations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=376;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=450;
 
 --
 -- Limiti per le tabelle scaricate
